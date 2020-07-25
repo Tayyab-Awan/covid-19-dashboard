ea1034f6205495c821502c6b7500cb8b284340b8
Tayyab Awan <tayyabaliawan1997@gmail.com>
2020-07-25 22:02:06 +0500

    Removed testing text from Heaer.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import './style/Header.css';
 export const Header = () => {
     return (
         <div className="header-container">
-            <h1>Covid-19 Outbreak Dashboard 2020</h1>
+            <h1>Covid-19 Outbreak Dashboard</h1>
         </div>
     )
 }
